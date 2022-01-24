d10fbc106d75e3556a402c34e93fa3a447f055ed
Guillaume <56391911+GuillaumeDupuy@users.noreply.github.com>
2022-01-24 23:58:48 +0100

    Update contact_validation.php

--- contact_validation.php
+++ contact_validation.php
@@ -1,7 +1,7 @@
 <?php include("assets/inc/header.inc.php"); ?>
 
     <div class='sucess'>
-            <h3>Vous avez envoyer votre message avec succés.</h3>
+            <h3>Vous avez envoyer votre message avec succès.</h3>
             <p>Cliquez ici pour retourner <a href='index.php'>à la page d'accueil</a></p>
     </div>
 
