fd83236049c959db436aadea729f1df0e0c8a4ed
Guillaume <56391911+GuillaumeDupuy@users.noreply.github.com>
2022-03-14 15:45:15 +0100

    Update data.inc.php

--- assets/inc/data.inc.php
+++ assets/inc/data.inc.php
@@ -1,5 +1,5 @@
 <?php
 
-$pdo = new PDO("mysql:host=localhost;dbname=u992063229_portfolio", "u992063229_guillaumedupuy", "Justinou93_", array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+$pdo = new PDO("mysql:host=localhost;dbname=~", "~", "~", array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 
 ?>
