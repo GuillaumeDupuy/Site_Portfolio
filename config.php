1a98f16bdf72352ff4850dac6cd202ddea70ba40
Guillaume <56391911+GuillaumeDupuy@users.noreply.github.com>
2022-03-14 15:44:02 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Informations d'identification
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'u992063229_guillaumedupuy');
-define('DB_PASSWORD', 'Justinou93_');
-define('DB_NAME', 'u992063229_portfolio');
+define('DB_USERNAME', '~');
+define('DB_PASSWORD', '~');
+define('DB_NAME', '~');
  
 // Connexion à la base de données MySQL 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
